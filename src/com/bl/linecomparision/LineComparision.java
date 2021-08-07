1e6a192c8213792ac25d7deab533f5fc88a7853e
vangapellisrividyam <vangapellisreevidya@gmail.com>
2021-08-07 15:45:07 +0530

    my first commit

---
+++ src/com/bl/linecomparision/LineComparision.java
@@ -0,0 +1,7 @@
+package com.bl.linecomparision;
+
+public class LineComparision {
+	public static void main(String[] args) {
+		System.out.println("Welcome to line comparision computation program on branch");
+	}
+}
