92d3383f43528d5d4d1da70cb187af9e49ac2c42
vangapellisrividyam <vangapellisreevidya@gmail.com>
2021-08-07 16:14:55 +0530

    my fifth commit

--- src/com/bl/linecomparision/LineComparision.java
+++ src/com/bl/linecomparision/LineComparision.java
@@ -2,35 +2,31 @@ package com.bl.linecomparision;
 
 public class LineComparision {
 
-	public static void Linelength1(int x1, int x2, int y1, int y2, int Linelength1) {
+	private int x1, x2, y1, y2;
 
-		Linelength1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+	public Integer Linelength() {
 
-		System.out.println(" " + Linelength1);
+		Integer Linelength = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println(" " + Linelength);
+
+		return Linelength;
 	}
 
-	public static void Linelength2(int x3, int x4, int y3, int y4, int Linelength2) {
-		Linelength2 = (int) Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+	public LineComparision(int x1, int x2, int y1, int y2) {
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
 
-		System.out.println(" " + Linelength2);
 	}
-
-	Object Linelength2;
-	private Object Linelength1;
 
 	public static void main(String[] args) {
-		LineComparision le1 = new LineComparision();
-
-		LineComparision le2 = new LineComparision();
-		LineComparision.Linelength1(1, 2, 1, 2, 0);
-		LineComparision.Linelength2(1, 2, 1, 2, 0);
-		if (le1.Linelength1 == le2.Linelength2)
-
-		{
-			System.out.println("length is equal ");
-		} else {
-			System.out.println("length is not equal");
-		}
+		LineComparision le1 = new LineComparision(1, 2, 1, 2);
+		LineComparision le2 = new LineComparision(1, 2, 1, 2);
+		Integer Linelength1 = le1.Linelength();
+		Integer Linelength2 = le2.Linelength();
+		System.out.println(Linelength1.compareTo(Linelength2));
 
 	}
 
