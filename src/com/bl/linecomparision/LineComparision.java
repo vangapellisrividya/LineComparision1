44a42b3be871d7da40c8a6b09a91798f629235e4
vangapellisrividyam <vangapellisreevidya@gmail.com>
2021-08-07 15:53:08 +0530

    my second commit

--- src/com/bl/linecomparision/LineComparision.java
+++ src/com/bl/linecomparision/LineComparision.java
@@ -1,7 +1,15 @@
 package com.bl.linecomparision;
 
 public class LineComparision {
+	public static void Linelength(int x1, int x2, int y1, int y2, int Linelength) {
+
+		Linelength = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+		System.out.println(" " + Linelength);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to line comparision computation program on branch");
+		Linelength(1, 2, 1, 2, 0);
+
 	}
 }
